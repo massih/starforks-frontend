eb2d4ace2799503ffeecf6675f437950a8a6fe4d
fabio <fabio.forcolin@gmail.com>
2021-12-26 14:30:05 +0100

    Remove dashboard module
    - Fix Recipies UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { NewRecipeComponent } from './new-recipe/new-recipe.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipesComponent } from './recipes/recipes.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent },
   { path: 'new-recipe', component: NewRecipeComponent },
   { path: 'detail/:id', component: RecipeDetailComponent}
