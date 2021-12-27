13d9c7813083d9c090fcb2cfbce1d7fbdd8f476f
Massih <massih.z@gmail.com>
2021-12-27 14:04:29 +0100

    Add shadow and spacing to reciepe list and details
    - WIP upload image

--- src/app/new-recipe/new-recipe.component.ts
+++ src/app/new-recipe/new-recipe.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Pipe, PipeTransform } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { RecipeType } from '../enums/recipeTypeEnum';
 import { Recipe } from '../recipe';
 import { RecipeService } from '../recipe.service';
@@ -12,7 +12,7 @@ import { RecipeService } from '../recipe.service';
 export class NewRecipeComponent implements OnInit {
   recipeTypes = RecipeType;
   recipeTypeNumbers: number[] = [];
-  recipe1 = null;
+  recipeImage: File | undefined;
 
   emptyRecipe = (): Recipe => ({ author: "", createdAt: "", id: 0, ingredients: "", name: "", picture: "", steps: "", type: RecipeType.Starter });
 
@@ -22,13 +22,21 @@ export class NewRecipeComponent implements OnInit {
   constructor(private recipeService: RecipeService) {
     this.recipeTypeNumbers = Object.keys(this.recipeTypes).map(k => Number(k)).filter(k => !Number.isNaN(k));
   }
+
+  onChange(event): void {
+    console.log("file changed");
+    this.recipeImage = event.target.files[0];
+  }
+
   submit(): void {
     console.log(this.newRecipe)
-    this.recipeService.saveRecipe(this.newRecipe).subscribe(id => console.log(id))
+    if (!this.recipeImage) {
+      console.log('file not selected');
+      return;
+    }
+    this.recipeService.saveRecipe(this.newRecipe, this.recipeImage).subscribe(id => console.log(id));
   }
+
   ngOnInit(): void {
-
   }
-
-
 }
