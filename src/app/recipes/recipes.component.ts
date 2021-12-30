e6ea56edbf2d00bab15e3fe92460130564054a33
fabio <fabio.forcolin@gmail.com>
2021-12-30 11:36:31 +0100

    Add search bar

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -2,21 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../recipe.service';
 import { RecipeDetails } from '../recipe-detail/recipe-details';
 
+
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
+
 export class RecipesComponent implements OnInit {
   recipes: RecipeDetails[] = [];
+  searchWords: string = "";
 
-  getRecipes(): void {
-    this.recipeService.getRecipes().subscribe(recipes => this.recipes = recipes);
+  getRecipes(searchWords: string): void {
+    this.recipeService.getRecipes(searchWords).subscribe(recipes => this.recipes = recipes);
   }
 
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
-    this.getRecipes();
+    this.getRecipes(this.searchWords);
+  }
+
+  public onSearchTypingFinished() {
+    let wordSearch = this.searchWords;
+    setTimeout(() => {
+      if (wordSearch == this.searchWords) {
+        this.getRecipes(this.searchWords);
+      }
+    }, 1000);
   }
 }
