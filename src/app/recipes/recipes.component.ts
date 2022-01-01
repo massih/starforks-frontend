b1cdfb6b0cea7124364dd5aea27b9699e2b9d041
fabio <fabio.forcolin@gmail.com>
2022-01-01 18:49:04 +0100

    Add progress bar

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../recipe.service';
 import { RecipePreview } from './recipe-preview';
+import { PaginatedRecipes } from './paginated-recipe';
 import { DomSanitizer } from "@angular/platform-browser";
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-recipes',
@@ -16,12 +18,17 @@ export class RecipesComponent implements OnInit {
   pageSize: number = 2;
   page: number = 1;
   previousPage = 0;
-  
+  progress: number = 0;
+  loading: boolean = false;
+
   getRecipes(searchWords: string): void {
+    this.loading = true
     let skip = (this.page - 1) * this.pageSize;
-    this.recipeService.getRecipes(searchWords, skip, this.pageSize).subscribe(paginatedRecipes => {
+    this.recipeService.getRecipes(searchWords, skip, this.pageSize)
+    .subscribe(paginatedRecipes => {
       this.recipes = this.parseRecipes(paginatedRecipes.recipes)
       this.total = paginatedRecipes.total;
+      this.loading = false;
     });
 
   }
@@ -32,7 +39,7 @@ export class RecipesComponent implements OnInit {
     this.getRecipes(this.searchWords);
   }
   parseRecipes(recipes: RecipePreview[]): RecipePreview[] {
-    
+
     let parsedRecipes: RecipePreview[] = recipes;
     for (let parsedRecipe of parsedRecipes) {
       parsedRecipe.picture = this.sanitizer.bypassSecurityTrustResourceUrl('data:image/png;base64,' + parsedRecipe.picture);
