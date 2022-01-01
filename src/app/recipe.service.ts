76d0e607a30dd586ae89988fa7fc8db4bce1a215
fabio <fabio.forcolin@gmail.com>
2022-01-01 16:48:52 +0100

    Fix merge issue

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -5,7 +5,7 @@ import {HttpClient, HttpEvent, HttpHeaders, HttpParams} from '@angular/common/ht
 import { catchError } from 'rxjs/operators';
 import { EnvService } from './env.service';
 import { RecipeDetails } from './recipe-detail/recipe-details';
-import { PaginatedRecipe } from './recipes/paginated-recipe';
+import { PaginatedRecipes } from './recipes/paginated-recipe';
 
 @Injectable({
   providedIn: 'root'
@@ -31,16 +31,16 @@ export class RecipeService {
       );
   }
 
-  getRecipes(searchWords: string, skip: number, limit: number): Observable<PaginatedRecipe> {
+  getRecipes(searchWords: string, skip: number, limit: number): Observable<PaginatedRecipes> {
     return this.http
-      .get<PaginatedRecipe>(this.backendUrl + "/", {
+      .get<PaginatedRecipes>(this.backendUrl + "/", {
         params: new HttpParams()
         .set('searchWords', searchWords)
         .set('skip', skip)
         .set('limit', limit)
     })
       .pipe(
-        catchError(this.handleError<PaginatedRecipe>('getRecipes', undefined))
+        catchError(this.handleError<PaginatedRecipes>('getRecipes', undefined))
       );
   }
 
--- src/app/recipes/recipe-preview.ts
+++ src/app/recipes/recipe-preview.ts
@@ -3,6 +3,6 @@ import { RecipeType } from "../enums/recipeTypeEnum";
 export interface RecipePreview {
     id: string
     name: string;
-    picture: string;
+    picture: string | any;
     type: RecipeType;
 }
